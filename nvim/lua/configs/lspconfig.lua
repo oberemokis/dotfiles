5dbaaf5c7b098da01be8881418d20cd7346f8b8d
Ilya Oberemok <oberemokis@proton.me>
2026-08-31 18:01:35 +0300

    Перенос настройки LSP на vim.lsp.config
    Фреймворк require("lspconfig") объявлен устаревшим, ругался при каждом старте
    и будет вырезан в v3.0.0. Серверы описаны таблицей и включаются через
    vim.lsp.enable, дефолты для каждого приходят из lsp/ в nvim-lspconfig.
    
    on_attach, capabilities и on_init убраны из каждого сервера: NvChad вешает
    LspAttach в defaults() и задаёт остальное через vim.lsp.config("*").
    
    Для eslint сохранено прежнее поведение привязки к корню по файлам конфига,
    иначе он подхватил бы монорепный root_dir из поставки и стартовал бы в
    проектах без eslint.

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,150 +1,58 @@
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
+local servers = {
+  html = {},
+  cssls = {},
+  tailwindcss = {},
+  volar = {},
+  jsonls = {},
+  yamlls = {},
+  graphql = {},
+  prismals = {},
+  bashls = {},
+  dockerls = {},
+  docker_compose_language_service = {},
+  pyright = {},
+  dartls = {},
+  clangd = {},
+  gopls = {},
+  jdtls = {},
+  groovyls = {},
 
-local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
-
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
-end
-
--- typescript | javascript | vue
-lspconfig.ts_ls.setup {
-  -- on_init = on_init,
-  -- capabilities = capabilities,
-  init_options = {
-    plugins = {
-      {
-        name = "@vue/typescript-plugin",
-        location = vim.fn.expand("~/.nvm/versions/node/v22.8.0/lib/node_modules/@vue/language-server"),
-        languages = { "javascript", "typescript", "vue" },
+  ts_ls = {
+    init_options = {
+      plugins = {
+        {
+          name = "@vue/typescript-plugin",
+          location = vim.fn.expand "~/.nvm/versions/node/v22.8.0/lib/node_modules/@vue/language-server",
+          languages = { "javascript", "typescript", "vue" },
+        },
       },
     },
+    filetypes = {
+      "javascript",
+      "javascriptreact",
+      "javascript.jsx",
+      "typescript",
+      "typescriptreact",
+      "typescript.tsx",
+      "vue",
+    },
   },
-  filetypes = {
-    "javascript",
-    "javascriptreact",
-    "javascript.jsx",
-    "typescript",
-    "typescriptreact",
-    "typescript.tsx",
-    "vue",
+
+  eslint = {
+    root_dir = function(bufnr, on_dir)
+      on_dir(vim.fs.root(bufnr, {
+        "eslint.config.js",
+        "eslint.config.mjs",
+        ".eslintrc.js",
+        ".eslintrc.json",
+        ".eslintrc",
+      }))
+    end,
   },
 }
 
--- tailwindcss
-lspconfig.tailwindcss.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
+for name, opts in pairs(servers) do
+  vim.lsp.config(name, opts)
+end
 
--- python
-lspconfig.pyright.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
--- dart
-lspconfig.dartls.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
--- cpp | c
-lspconfig.clangd.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
--- go
-lspconfig.gopls.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
--- java
-lspconfig.jdtls.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
--- vue
-require 'lspconfig'.volar.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
--- groovy
-lspconfig.groovyls.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
--- eslint
-lspconfig.eslint.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-  root_dir = require("lspconfig").util.root_pattern("eslint.config.js", ".eslintrc.js", ".eslintrc.json", ".eslintrc", "eslint.config.mjs"),
-}
-
--- json | jsonc
-lspconfig.jsonls.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
--- yaml
-lspconfig.yamlls.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
--- docker
-lspconfig.dockerls.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
--- docker compose
-lspconfig.docker_compose_language_service.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
--- graphql
-lspconfig.graphql.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
--- bash
-lspconfig.bashls.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
--- prismals
-lspconfig.prismals.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
+vim.lsp.enable(vim.tbl_keys(servers))
